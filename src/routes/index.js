f7831ab60deb79581949c842b54634b49e84b147
Daniel-Robles1 <jovaban43@gmail.com>
2025-10-06 13:10:41 -0600

    catalogos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,31 @@
-import { Router } from 'express';
-import authRoutes from './auth.routes.js';
-import usuarioRoutes from './usuario.routes.js';
-import rolRoutes from './rol.routes.js';
-import permisoRoutes from './permiso.routes.js';
-import catalogoRoutes  from './catalogo.routes.js'
-import empresaRoutes from './empresa.routes.js';
-import periodoRoutes from './periodo.routes.js';
-import impuestoRoutes from './impuesto.routes.js';
-import ejercicios from './ejercicios.routes.js';
-import centroCosto from './centro-costo.routes.js';
+// routes/index.js
+ import { Router } from 'express';
+ import authRoutes from './auth.routes.js';
+ import usuarioRoutes from './usuario.routes.js';
+ import rolRoutes from './rol.routes.js';
+ import permisoRoutes from './permiso.routes.js';
+ import catalogoRoutes  from './catalogo.routes.js'
+ import empresaRoutes from './empresa.routes.js';
+ import periodoRoutes from './periodo.routes.js';
+ import impuestoRoutes from './impuesto.routes.js';
+ import ejercicios from './ejercicios.routes.js';
+import cuentaRoutes from './cuenta.routes.js';
+import centroCostoRoutes from './centro-costo.routes.js';
 
-const router = Router();
+ const router = Router();
 
-router.use('/auth', authRoutes);
-router.use('/usuarios', usuarioRoutes);
-router.use('/roles', rolRoutes);
-router.use('/permisos', permisoRoutes);
-router.use('/catalogos', catalogoRoutes);
-router.use('/empresas', empresaRoutes);
-router.use('/periodos', periodoRoutes);
-router.use('/impuestos', impuestoRoutes);
-router.use('/ejercicios', ejercicios);
-router.use('/centros', centroCosto);
+ router.use('/auth', authRoutes);
 
-export default router;
+ router.use('/usuarios', usuarioRoutes);
+ router.use('/roles', rolRoutes);
+ router.use('/permisos', permisoRoutes);
+ router.use('/catalogos', catalogoRoutes);
+ router.use('/empresas', empresaRoutes);
+ router.use('/periodos', periodoRoutes);
+ router.use('/impuestos', impuestoRoutes);
+ router.use('/ejercicios', ejercicios);
+router.use('/cuentas', cuentaRoutes);
+
+router.use('/centros', centroCostoRoutes);
+
+ export default router;
